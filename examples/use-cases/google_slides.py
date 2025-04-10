fc91e70e9c714364db5435bacf3d032b0c72de57
os1rix <silvoniemioskari@gmail.com>
2025-04-10 23:53:20 +0300

    switching to using deepseek

--- examples/use-cases/google_slides.py
+++ examples/use-cases/google_slides.py
@@ -2,27 +2,29 @@ import os
 import sys
 
 from browser_use.browser.context import BrowserContext
+from pydantic import SecretStr
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import asyncio
 
 import pyperclip
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
+from langchain_deepseek import ChatDeepSeek
 
 from browser_use import ActionResult, Agent, Controller
 from browser_use.browser.browser import Browser, BrowserConfig
 
 browser = Browser(
 	config=BrowserConfig(
-		browser_binary_path='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+		browser_binary_path="C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
 	),
 )
 
-# Load environment variables
 load_dotenv()
-if not os.getenv('OPENAI_API_KEY'):
-	raise ValueError('OPENAI_API_KEY is not set. Please add it to your environment variables.')
+api_key = os.getenv('DEEPSEEK_API_KEY', '')
+if not api_key:
+	raise ValueError('DEEPSEEK_API_KEY is not set')
 
 
 controller = Controller()
@@ -33,26 +35,30 @@ def is_google_slide(page) -> bool:
 
 
 @controller.registry.action('Google Slides: Open a slides presentation')
-async def open_google_slide(browser: BrowserContext, google_sheet_url: str):
+async def open_google_slide(browser: BrowserContext, google_slide_url: str):
 	page = await browser.get_current_page()
-	if page.url != google_sheet_url:
-		await page.goto(google_sheet_url)
+	if page.url != google_slide_url:
+		await page.goto(google_slide_url)
 		await page.wait_for_load_state()
 	if not is_google_slide(page):
 		return ActionResult(error='Failed to open Google Slides, are you sure you have permissions to access this presenation?')
-	return ActionResult(extracted_content=f'Opened Google Slides {google_sheet_url}', include_in_memory=False)
+	return ActionResult(extracted_content=f'Opened Google Slides {google_slide_url}', include_in_memory=False)
 
 async def main():
-	async with await browser.new_context() as context:
-		model = ChatOpenAI(model='gpt-4o')
+		model = ChatDeepSeek(
+			model='deepseek-chat',
+			api_key=SecretStr(api_key),
+		)
 
 		agent = Agent(
 			task="""
-				task placeholder
+				go to google slides: https://docs.google.com/presentation/d/1hUeZ-tsSRbi4bTu0f9ZcrRF-rzMp599FJ9rL91XdOEI/edit?slide=id.p#slide=id.p
 			""",
 			llm=model,
-			browser_context=context,
+			browser=browser,
 			controller=controller,
+			use_vision=False,
+			enable_memory=False
 		)
 		await agent.run()
 
